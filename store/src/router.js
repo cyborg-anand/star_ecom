eefcbeb828a143a444e252e739ba36bd6d2c0fea
cyborg-anand <anandncs@gmail.com>
2024-12-13 17:27:09 +0530

    day4 EOD

---
+++ store/src/router.js
@@ -0,0 +1,44 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import { session } from './data/session'
+import { userResource } from '@/data/user'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('@/pages/Home.vue'),
+    meta: {
+      requiresLogin:false
+    }
+  },
+  {
+    path: '/checkout',
+    name: 'CheckoutPage',
+    meta:{
+      requiresLogin:true
+    }
+  }
+
+]
+
+let router = createRouter({
+  history: createWebHistory('/store'),
+  routes,
+})
+
+router.beforeEach(async (to, from, next) => {
+  let isLoggedIn = session.isLoggedIn
+  try {
+    await userResource.promise
+  } catch (error) {
+    isLoggedIn = false
+  }
+    
+  if (to.meta.requiresLogin && !isLoggedIn){
+    //throw them to loginpge
+    window.location.href="/login?redirect-to=/store";
+  }
+   next();
+})
+
+export default router
